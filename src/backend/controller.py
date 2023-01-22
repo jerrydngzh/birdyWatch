c849ce5119ea54433372e8615020928838255919
jerrydengz <jerrydeng67@gmail.com>
2023-01-22 01:07:22 -0800

    integrate main function call for image labled data

--- src/backend/controller.py
+++ src/backend/controller.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
+from .notMain import make_birds
 
 app = Flask(__name__)
 
-@app.route("")
+# what it's supposed to be like:
+# 1. POST -- FE sends request w/ image body, respond with fully formed data
+
+    # a. POST -- google cloud -> response img w/ bounding box, & cropped
+
+    # b. call tensorflow library -> label image, determine bird species
+
+    # c. GET -- call wiki -> get flavour text
+
+    # d. send back to FE as response -> flavour text / image labeled data as json string
+
+@app.route("/whosThatBirdmon", methods = ['POST'])
+def whosThatBirdmon():
+
+    # mega function that does everything 
+    imagePath = request.data
+    ImageLabeledBirdsList = make_birds(imagePath)
+
+    # serialize ImageLabeled Class 
+    response = ImageLabeledBirdsList.dumps()
+    return response
+
+
+@app.route("/")
 def base():
     return "base of the flask server"
 
+if __name__ == "__main__":
+    app.run()
